a71c52cd8ac86bba767a7c7c67b439352e38aae5
Obaydah <obaydahmp@hotmail.com>
2020-05-09 01:42:14 +0200

    NoneType fix

--- script.py
+++ script.py
@@ -1,21 +1,18 @@
 import time
 from csv import writer
-import bs4
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
-"""
-def write_viewers_to_csv(page_source):
-    soup = bs4.BeautifulSoup(page_source, 'html.parser')
-    views = soup.find("span", {"class":"view-count"})
-    views_splitted = views.getText().split(" ")
+
+def write_viewers_to_csv(views):
+    views_splitted = views.split(" ")
 
     with open("./views.csv", 'a+', newline='') as file:
         csv_writer = writer(file)
         csv_writer.writerow([time.strftime("%Y-%m-%d %H:%M"), views_splitted[0]])
-"""
+
 options = Options()
 options.headless = True
 browser = webdriver.Firefox(options=options)
@@ -28,10 +25,13 @@ try:
 
     currentTime = time.strftime("%Y%m%d-%H%M")
     browser.save_screenshot(f"./screenshots/screenshot_{currentTime}.png")
-    #write_viewers_to_csv(browser.page_source)
-    time.sleep(5)
+    views = browser.find_element_by_class_name("view-count")
 
+    write_viewers_to_csv(views.text)
+    time.sleep(5)
+    
     print("done")
 finally:
     browser.quit()
+    print("driver disposed")
 
