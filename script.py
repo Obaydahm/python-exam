a6c46cd73674d03369d3b88958a58f4284b3b8a1
Obaydah <obaydahmp@hotmail.com>
2020-05-09 04:35:47 +0200

    progress

--- script.py
+++ script.py
@@ -18,20 +18,25 @@ chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
-chrome_options.add_argument('window-size=1920x1080')
+chrome_options.add_argument('--autoplay-policy=no-user-gesture-required')
+
+
 browser = webdriver.Chrome(options=chrome_options)
 #options = Options()
 #options.headless = True
 #browser = webdriver.Firefox(options=options)
 try:
     browser.get('https://www.youtube.com/watch?v=mRe-514tGMg')
-    time.sleep(5)
+    time.sleep(15)
 
-    ActionChains(browser).send_keys(Keys.SPACE)
-    time.sleep(10)
+    #ActionChains(browser).send_keys(Keys.F11)
+    #time.sleep(5)
+    
+    #player = browser.find_element_by_class_name("ytp-fullscreen-button")
+    #ActionChains(browser).double_click(player).perform()
 
-    browser.find_element_by_class_name("ytp-fullscreen-button").click()
-    time.sleep(10)
+    #browser.find_element_by_class_name("ytp-fullscreen-button").click()
+    #time.sleep(10)
 
     currentTime = time.strftime("%Y%m%d-%H%M")
     browser.save_screenshot(f"./screenshots/screenshot_{currentTime}.png")
