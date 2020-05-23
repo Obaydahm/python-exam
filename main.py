022b69717be323663fa5b477a88feefc3f683362
Obaydah <obaydahmp@hotmail.com>
2020-05-23 14:49:47 +0200

    clean up

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import DetectExam
 import flaskserver
 
 def main():
+    #Download stream and start detection
     DetectExam
+
+    #Insert result into database
     SQL.insert_detected_into_database()
+
+    #Plot data
     SQL.detected_to_bar_chart()
 
 if __name__ == "__main__":
